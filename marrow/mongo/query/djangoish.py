d7aba7c54f1897c8ec31de00d0e26d1d4a70eb34
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-20 05:32:06 -0400

    Initial Djanglish sort and projection helpers.

--- marrow/mongo/query/djangoish.py
+++ marrow/mongo/query/djangoish.py
@@ -45,6 +45,7 @@ from __future__ import unicode_literals
 import operator
 
 from collections import deque
+from pymongo import ASCENDING, DESCENDING
 
 from ..util.compat import unicode
 from . import Ops
@@ -194,6 +195,55 @@ def F(Document, __raw__=None, **filters):
 		ops &= op
 	
 	return ops
+
+
+def S(Document, *fields):
+	"""Generate a MongoDB sort order list using the Django ORM style."""
+	
+	result = []
+	
+	for field in fields:
+		if isinstance(field, tuple):  # Unpack existing tuple.
+			field, direction = field
+			result.append((field, direction))
+			continue
+		
+		direction = ASCENDING
+		field = field.replace('__', '.')
+		
+		if field[0] == '-':
+			direction = DESCENDING
+		
+		if field[0] in ('+', '-'):
+			field = field[1:]
+		
+		result.append((field, direction))
+	
+	return result
+
+
+def P(Document, *fields, __always__=None):
+	"""Generate a MongoDB projection dictionary using the Django ORM style."""
+	
+	__always__ = __always__ if __always__ else set()
+	projected = set()
+	omitted = set()
+	
+	for field in fields:
+		if field[0] in ('-', '!'):
+			omitted.add(field[1:])
+		elif field[0] == '+':
+			projected.add(field[1:])
+		else:
+			projected.add(field)
+	
+	if not projected:
+		names = Document.__projection__.keys() if Document.__projection__ else Document.__fields__.keys()
+		projected = {name for name in (names - omitted)}
+	
+	projected |= __always__
+	
+	return {name: True for name in projected}
 
 
 def U(Document, **update):
