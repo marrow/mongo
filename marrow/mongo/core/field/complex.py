4e604094ad157691d359183b44170908628452a7
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-22 08:40:53 -0500

    Alias implementation.

--- marrow/mongo/core/field/complex.py
+++ marrow/mongo/core/field/complex.py
@@ -197,3 +197,53 @@ class PluginReference(Field):
 			raise ValueError(repr(value) + ' object is not a known plugin for namespace "' + namespace + '".')
 		
 		return plugins.get(value, canon(value))
+
+
+class Alias(Attribute):
+	"""Reference a field, potentially nested, elsewhere in the document.
+	
+	This provides a shortcut for querying nested fields, for example, in GeoJSON, to more easily access the latitude
+	and longitude:
+	
+		class Point(Document):
+			kind = String('type', default='point', assign=True)
+			coordinates = Array(Number(), default=lambda: [0, 0], assign=True)
+			latitude = Alias('coordinates.1')
+			longitude = Alias('coordinates.0')
+	
+	You can now read and write `latitude` and `longitude` on instances of `Point`, as well as query the nested values
+	through class attribute access.
+	"""
+	
+	path = Attribute()
+	path.__sequence__ -= 10000
+	
+	def __init__(self, path):
+		super(Alias, self).__init__(path=path)
+	
+	def __fixup__(self, document):
+		"""Called after an instance of our Field class is assigned to a Document."""
+		self.__document__ = proxy(document)
+	
+	def __get__(self, obj, cls=None):
+		if obj is None:
+			return traverse(self.__document__, self.path)
+		
+		return traverse(obj, self.path)
+	
+	def __set__(self, obj, value):
+		parts = self.path.split('.')
+		final = parts.pop()
+		current = obj
+		
+		for part in parts:
+			if part.lstrip('-').isdigit():
+				current = current[int(part)]
+				continue
+			
+			current = getattr(current, part)
+		
+		if final.lstrip('-').isdigit():
+			current[int(final)] = value
+		else:
+			setattr(current, final, value)
