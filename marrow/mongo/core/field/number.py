07023141d69eace7b3f9d5fc74a6e64fa42bb704
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-24 22:59:35 -0500

    Unused import and unused API exclusions.

--- marrow/mongo/core/field/number.py
+++ marrow/mongo/core/field/number.py
@@ -2,10 +2,9 @@
 
 from numbers import Number as NumberABC
 from bson.int64 import Int64
-from decimal import Decimal as dec
 
 from . import Field
-from ...util.compat import str, unicode
+from ...util.compat import unicode
 
 
 class Number(Field):
@@ -28,19 +27,19 @@ class Number(Field):
 class Double(Number):
 	__foreign__ = 'double'
 	
-	def to_foreign(self, obj, name, value):
+	def to_foreign(self, obj, name, value):  # noqa
 		return float(value)
 
 
 class Integer(Number):
 	__foreign__ = 'integer'
 	
-	def to_foreign(self, obj, name, value):
+	def to_foreign(self, obj, name, value):  # noqa
 		return int(value)
 
 
 class Long(Number):
 	__foreign__ = 'long'
 	
-	def to_foreign(self, obj, name, value):
+	def to_foreign(self, obj, name, value):  # noqa
 		return Int64(int(value))
