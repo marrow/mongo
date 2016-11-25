ee6499de7a3df615c4f7d23037668056354dbfc6
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-24 23:01:13 -0500

    Import * support.

--- marrow/mongo/__init__.py
+++ marrow/mongo/__init__.py
@@ -7,3 +7,5 @@ from .util import Registry
 
 document = sys.modules['marrow.mongo.document'] = Registry('marrow.mongo.document')
 field = sys.modules['marrow.mongo.field'] = Registry('marrow.mongo.field')
+
+__all__ = ['Document', 'Field', 'Index', 'document', 'field']
