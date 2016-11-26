87b4edbeb4ab372e62aac3c8bdd2abe650acf8d2
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-26 02:26:50 -0500

    Additional global imports.

--- marrow/mongo/__init__.py
+++ marrow/mongo/__init__.py
@@ -3,9 +3,20 @@
 import sys
 
 from .core import Document, Field, Index, __version__  # noqa
+from .param import F, P, S, U
 from .util import Registry
 
 document = sys.modules['marrow.mongo.document'] = Registry('marrow.mongo.document')
 field = sys.modules['marrow.mongo.field'] = Registry('marrow.mongo.field')
 
-__all__ = ['Document', 'Field', 'Index', 'document', 'field']
+__all__ = [
+	'Document',
+	'F',
+	'Field',
+	'Index',
+	'P',
+	'S',
+	'U',
+	'document',
+	'field',
+]
