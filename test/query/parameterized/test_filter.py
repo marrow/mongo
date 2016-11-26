13ddd70422b11d300a5a6b19e53c6f099d629277
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-25 21:42:52 -0500

    Move to isolation.

---
+++ test/query/parameterized/test_filter.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from marrow.mongo.query.parameterized import F, P, S, U
+
+F  # Filter Document
+S  # Sort Ordering
+P  # Projection Document
+U  # Update Document
