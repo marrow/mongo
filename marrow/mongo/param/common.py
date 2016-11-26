3358c279731aa87166c18e933570805ed16ea97c
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-26 02:50:24 -0500

    First test.

--- marrow/mongo/param/common.py
+++ marrow/mongo/param/common.py
@@ -4,6 +4,8 @@
 
 from __future__ import unicode_literals
 
+from marrow.package.loader import traverse
+
 
 def _deferred_method(name, _named=None, **kw):
 	def _deferred_method_inner(self, other):
@@ -11,18 +13,24 @@ def _deferred_method(name, _named=None, **kw):
 			if isinstance(_named, tuple):
 				assert len(_named) == len(other), "Incorrect number of arguments."
 				values = iter(other)
+				
 				for i in _named:
 					kw[i] = next(values)
+			
 			else:
 				kw[_named] = other
+			
 			return getattr(self, name)(**kw)
+		
 		return getattr(self, name)(other, **kw)
+	
 	return _deferred_method_inner
 
 
 def _operator_choice(conversion, lookup, **kw):
 	def _operator_choice_inner(self, other):
 		return lookup[conversion(other)](self, **kw)
+	
 	return _operator_choice_inner
 
 
@@ -31,15 +39,12 @@ def _process_arguments(Document, prefixes, suffixes, arguments):
 		prefix, _, nname = name.partition('__')
 		if prefix in prefixes:
 			name = nname
-		else:
-			prefix = None
 		
 		nname, _, suffix = name.rpartition('__')
 		if suffix in suffixes:
 			name = nname
-		else:
-			suffix = None
 		
-		field = traverse(Document, name.replace('__', '.'))
+		field = traverse(Document, name.replace('__', '.'))  # Find the target field.
+		value = field.transformer.foreign(value, (field, Document))  # Typecast the value to MongoDB-safe as needed.
 		
-		yield prefixes.get(prefix, None), suffixes.get(suffix, None), field, value
+		yield prefixes.get(prefix or None, None), suffixes.get(suffix, None), field, value
--- test/param/test_filter.py
+++ test/param/test_filter.py
@@ -2,6 +2,21 @@
 
 from __future__ import unicode_literals
 
-from marrow.mongo.param import F
+import pytest
 
-F  # Filter Document
+from marrow.mongo import Document, F, Field
+from marrow.mongo.query import Ops
+
+
+class TestParametricFilterConstructor(object):
+	@pytest.fixture()
+	def D(self):
+		class Sample(Document):
+			field = Field()
+			bar = Field()
+		return Sample
+	
+	def test_basic_equality_operator_default(self, D):
+		q = F(D, field=27)
+		assert isinstance(q, Ops)
+		assert q == {'field': 27}
