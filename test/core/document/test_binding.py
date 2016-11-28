07ba542adf4eee97b428e84ab7c3c9d1d81914cd
Alice Bevan-McGregor <alice@gothcandy.com>
2016-11-27 19:47:49 -0500

    Index construction tests.

--- test/core/document/test_binding.py
+++ test/core/document/test_binding.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 import pytest
 from pymongo.errors import WriteError
 
-from marrow.mongo import Document, Field
+from marrow.mongo import Document, Field, Index
 
 
 @pytest.fixture
@@ -25,6 +25,9 @@ def Sample(request):
 		__engine__ = {'mmapv1': {}}
 		
 		field = Field()
+		other = Field()
+		
+		_field = Index('field', background=False)
 	
 	return Sample
 
@@ -49,10 +52,14 @@ class TestDocumentBinding(object):
 		assert Sample.__bound__
 	
 	def test_create_collection(self, db, Sample):
-		assert Sample.create_collection(db).name == 'collection'
+		assert Sample.create_collection(db, indexes=False).name == 'collection'
 	
 	def test_create_collection_collection(self, db, Sample):
-		assert Sample.create_collection(db.foo, True).name == 'foo'
+		assert Sample.create_collection(db.foo, True, indexes=False).name == 'foo'
+	
+	def test_get_collection_failure(self, Sample):
+		with pytest.raises(TypeError):
+			Sample.get_collection(None)
 	
 	def test_validation(self, db, Sample):
 		if tuple((int(i) for i in db.client.server_info()['version'].split('.')[:3])) < (3, 2):
@@ -60,9 +67,23 @@ class TestDocumentBinding(object):
 		
 		Sample.__validate__ = 'strict'
 		Sample.__validator__ = {'field': {'$gt': 27}}
-		c = Sample.create_collection(db, True)
+		c = Sample.create_collection(db, True, indexes=False)
 		
 		c.insert_one(Sample(42))
 		
 		with pytest.raises(WriteError):
 			c.insert_one(Sample(12))
+	
+	def test_index_construction(self, db, Sample):
+		c = Sample.create_collection(db, True, indexes=False)
+		Sample.create_indexes(c, True)
+		
+		indexes = c.index_information()
+		assert '_field' in indexes
+		assert indexes['_field'] == {
+				'background': False,
+				'key': [('field', 1)],
+				'ns': 'test.collection',
+				'sparse': False,
+				'v': 1,
+			}
