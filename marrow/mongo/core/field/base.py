f579bfa6a12638a60378089fdecaaec4ce66f512
Alice Bevan-McGregor <alice@gothcandy.com>
2017-01-03 14:00:07 -0500

    String stripping and case conversion.

--- marrow/mongo/core/field/base.py
+++ marrow/mongo/core/field/base.py
@@ -17,8 +17,27 @@ class String(Field):
 	__foreign__ = 'string'
 	__disallowed_operators__ = {'#array'}
 	
+	strip = Attribute(default=False)
+	case = Attribute(default=None)
+	
 	def to_foreign(self, obj, name, value):  # pylint:disable=unused-argument
-		return unicode(value)
+		value = unicode(value)
+		
+		if self.strip is True:
+			value = value.strip()
+		elif self.strip:
+			value = value.strip(self.strip)
+		
+		if self.case in (1, True, 'u', 'upper'):
+			value = value.upper()
+		
+		elif self.case in (-1, False, 'l', 'lower'):
+			value = value.lower()
+		
+		elif self.case == 'title':
+			value = value.title()
+		
+		return value
 
 
 class Binary(Field):
--- test/field/test_base.py
+++ test/field/test_base.py
@@ -24,6 +24,36 @@ class FieldExam(object):
 
 class TestStringField(FieldExam):
 	__field__ = String
+	
+	def test_strip_basic(self, Sample):
+		Sample.field.strip = True
+		
+		inst = Sample('\tTest\n   ')
+		assert inst.field == 'Test'
+	
+	def test_strip_specific(self, Sample):
+		Sample.field.strip = '* '
+		
+		inst = Sample('** Test *')
+		assert inst.field == 'Test'
+	
+	def test_case_lower(self, Sample):
+		Sample.field.case = 'lower'
+		
+		inst = Sample('Test')
+		assert inst.field == 'test'
+	
+	def test_case_upper(self, Sample):
+		Sample.field.case = 'upper'
+		
+		inst = Sample('Test')
+		assert inst.field == 'TEST'
+	
+	def test_case_title(self, Sample):
+		Sample.field.case = 'title'
+		
+		inst = Sample('Test words')
+		assert inst.field == 'Test Words'
 
 
 class TestBinaryField(FieldExam):
