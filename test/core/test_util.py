e79fd974b52ff81607f2226d4cf1c93c65c2ae09
Alice Bevan-McGregor <alice@gothcandy.com>
2017-02-19 02:23:40 -0500

    Attribute adjustment correction.

--- test/core/test_util.py
+++ test/core/test_util.py
@@ -1,3 +1,22 @@
 # encoding: utf-8
 
-from marrow.mongo.util import *
+from marrow.mongo import Document, Field
+from marrow.mongo.util import adjust_attribute_sequence, utcnow
+
+
+class TestSequenceAdjustment(object):
+	def test_default_punt(self):
+		@adjust_attribute_sequence('last')
+		class Inner(Document):
+			last = Field()
+			first = Field()
+		
+		assert Inner('first', 'last').first == 'first'
+	
+	def test_promotion(self):
+		@adjust_attribute_sequence(-2, 'first')
+		class Inner(Document):
+			last = Field()
+			first = Field()
+		
+		assert Inner('first', 'last').first == 'first'
