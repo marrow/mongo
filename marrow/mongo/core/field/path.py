123bba60345acdaae3db2dcdfa1b1435f8f55b14
Alice Bevan-McGregor <alice@gothcandy.com>
2017-02-21 18:50:26 -0500

    Added Localized alias-alike field and blank line cleanup.

--- marrow/mongo/core/field/path.py
+++ marrow/mongo/core/field/path.py
@@ -11,7 +11,6 @@ except ImportError:
 	from pathlib2 import PurePosixPath as _Path
 
 
-
 class Path(String):
 	def to_native(self, obj, name, value):  # pylint:disable=unused-argument
 		return _Path(value)
