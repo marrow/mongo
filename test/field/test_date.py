03ad5a9e31127828ce7f14de61af80af20362624
Alice Bevan-McGregor <alice@gothcandy.com>
2017-05-17 13:36:35 -0400

    Add test for extraction of dates from ObjectIds.

--- test/field/test_date.py
+++ test/field/test_date.py
@@ -2,9 +2,17 @@
 
 from __future__ import unicode_literals
 
+from datetime import datetime
+
+from bson import ObjectId
 from common import FieldExam
 from marrow.mongo.field import Date
 
 
 class TestDateField(FieldExam):
 	__field__ = Date
+	
+	def test_date_like_oid(self, Sample):
+		oid = ObjectId('586846800000000000000000')
+		
+		assert Sample(oid).field.replace(tzinfo=None) == datetime(2017, 1, 1)
