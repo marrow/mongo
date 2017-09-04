cba8a78b12bc68be25c4c04781574b02dee58e3f
Alice Bevan-McGregor <alice@gothcandy.com>
2017-09-04 04:55:52 -0400

    Link field simplification.

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
 		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
 	
 	install_requires = [
+			'uri>=2.0.0,<3.0.0',  # Generic URI datastructure.
 			'marrow.schema>=1.2.0,<2.0.0',  # Declarative schema support.
 			'marrow.package>=1.1.0,<2.0.0',  # Plugin discovery and loading.
 			'pymongo>=3.2',  # We require modern API.
