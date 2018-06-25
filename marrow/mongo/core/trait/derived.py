817cdb1c006b666221e54bf40ccafc79874bbdd0
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2018-06-25 13:33:26 -0400

    Ability to promote and demote derive-(d/able) instances amongst member classes. (#55)
    * Initial implementation of promotion/demotion.
    * Deprecate Python 3.3 testing; unsupported by pytest now.
    * Imports simplification, promotion tests.
    * Remove catchlog; now in pytest core.

--- marrow/mongo/core/trait/derived.py
+++ marrow/mongo/core/trait/derived.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+from weakref import WeakValueDictionary
+
 from ... import Document, Index
 from ...field import PluginReference
 
@@ -7,7 +9,9 @@ from ...field import PluginReference
 class Derived(Document):
 	"""Access and store the class reference to a particular Document subclass.
 	
-	This allows for easy access to the magical `_cls` key as the `cls` attribute.
+	This allows for easy access to the magical `_cls` key as the `cls` attribute. This value will be populated
+	automatically.  This also allows for transformation (promotion/demotion) to a more specialized subclass or more
+	general parent class respectively.
 	"""
 	
 	__type_store__ = '_cls'  # The pseudo-field to store embedded document class references as.
@@ -19,6 +23,31 @@ class Derived(Document):
 	def __init__(self, *args, **kw):
 		"""Automatically derive and store the class path or plugin reference name."""
 		
+		kw.setdefault('cls', self.__class__)
 		super(Derived, self).__init__(*args, **kw)
+	
+	def _as(self, cls, update=False, preserve=True):
+		self.cls = cls
 		
-		self.cls = self.__class__
+		# if update:  TODO: Optionally actually update the record.
+		
+		return cls.from_mongo(self.__data__)
+	
+	def promote(self, cls, update=False, preserve=True):
+		"""Transform this record into an instance of a more specialized subclass."""
+		
+		if not issubclass(cls, self.__class__):
+			raise TypeError("Must promote to a subclass of " + self.__class__.__name__)
+		
+		return self._as(cls, update, preserve)
+	
+	def demote(self, cls=None, update=False, preserve=True):
+		"""Transform this record into an instance of a more generalized parent class.
+		
+		Defaults to the immediate parent class, if unambiguous (singular).
+		"""
+		
+		if not issubclass(self.__class__, cls):
+			raise TypeError("Must demote to a superclass of " + self.__class__.__name__)
+		
+		return self._as(cls, update, preserve)
--- test/trait/test_derived.py
+++ test/trait/test_derived.py
@@ -2,9 +2,13 @@
 
 from __future__ import unicode_literals
 
+import os
+
 import pytest
 
-from marrow.mongo.trait import Derived
+from marrow.mongo.field import Integer, String
+from marrow.mongo.trait import Derived, Queryable
+from marrow.schema.compat import pypy
 
 
 class TestDerived(object):
@@ -25,3 +29,105 @@ class TestDerived(object):
 	
 	def test_index(self):
 		assert '_cls' in self.Sample.__indexes__
+
+
+
+# From: https://github.com/MongoEngine/mongoengine/issues/167#issuecomment-297513842
+
+class Source(Derived, Queryable):
+	__collection__ = 'sources'
+
+class TextSource(Source):
+	author = String()
+	word_count = Integer()
+
+class Book(TextSource):
+	pages = Integer()
+
+class Journal(TextSource):
+	title = String()
+
+class Article(TextSource):
+	pass
+
+#        Source
+#          |
+#       TextSource
+#   /      |        \
+# Book  Journal  Article
+
+class TestPromotionDemotion(object):
+	def test_specialize(self):
+		a = Source()
+		b = a.promote(Journal)
+		assert isinstance(b, Journal)
+		
+		b.title = "Foo"
+		assert b['title'] == "Foo"
+	
+	def test_specialize_fail_underived_uncle(self):
+		a = Source()
+		
+		with pytest.raises(TypeError):
+			a.promote(Queryable)
+	
+	def test_specialize_fail_derived_uncle(self):
+		b = Book()
+		
+		with pytest.raises(TypeError):
+			b.promote(Journal)
+
+
+
+class Asset(Derived, Queryable):
+	id = String('_id')
+
+
+class Page(Asset):
+	content = String()
+	view = String(default='render')
+
+
+class Folder(Asset):
+	view = String(default='list')
+
+
+class Gallery(Folder):
+	thumbnail = Integer(default=320)
+	view = Folder.view.adapt(default='grid')
+
+
+class TestAssetPromotion(object):
+	# From original ticket description.
+	
+	def test_folder_gallery(self):
+		inst = Folder('sample')
+		
+		assert inst.id == 'sample'
+		assert inst.view == 'list'
+		
+		inst = inst.promote(Gallery)
+		
+		assert isinstance(inst, Gallery)
+		assert inst.id == 'sample'
+		assert inst.thumbnail == 320
+		assert inst.view == 'grid'
+	
+	def test_folder_page(self):
+		inst = Folder('sample')
+		
+		with pytest.raises(TypeError):
+			inst.promote(Page)
+	
+	def test_folder_asset(self):
+		inst = Folder('sample')
+		
+		assert inst.view == 'list'
+		
+		inst = inst.demote(Asset)
+		
+		assert isinstance(inst, Asset)
+		assert inst.id == 'sample'
+		
+		with pytest.raises(AttributeError):
+			inst.view
