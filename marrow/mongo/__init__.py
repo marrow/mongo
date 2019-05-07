69a46452305077ca70a43420dfdfa91266036b38
Alice Bevan-McGregor <alice@gothcandy.com>
2019-05-07 10:03:56 -0400

    Punt our own ObjectID up to the root package for easier importing.

--- marrow/mongo/__init__.py
+++ marrow/mongo/__init__.py
@@ -4,6 +4,7 @@ from .core import Document, Field, Index, __version__  # noqa
 from .query import Q, Ops, Filter, Update
 from .param import F, P, S, U
 from .util import Registry, utcnow
+from .util.oid import ObjectID
 
 document = sys.modules['marrow.mongo.document'] = Registry('marrow.mongo.document')
 field = sys.modules['marrow.mongo.field'] = Registry('marrow.mongo.field')
@@ -15,5 +16,6 @@ __all__ = [
 	'Q', 'Ops', 'Filter', 'Update',  # Filter qurey document, update document, and inline query operations.
 	'F', 'P', 'S', 'U',  # Parametric hepers.
 	'utcnow',  # Utility helper, e.g. to use as a default.
+	'ObjectID', # Cleanroom reimplenetation with legacy generation support.
 	'document', 'field', 'trait',  # Entry point (plugin) registries / namespace modules.
 ]
