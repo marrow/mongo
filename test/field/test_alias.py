2853689d85befff45da6d2e8b02b01311262349e
Alice Bevan-McGregor <alice@gothcandy.com>
2019-05-15 14:28:01 -0400

    Attribute names are now quoted in errors.

--- test/field/test_alias.py
+++ test/field/test_alias.py
@@ -36,7 +36,7 @@ class TestAliasDirect:
 			
 			assert len(w) == 1
 			assert issubclass(w[-1].category, DeprecationWarning)
-			assert 'via deprecated' in str(w[-1].message)
+			assert 'via "deprecated"' in str(w[-1].message)
 			assert 'xyzzy' in str(w[-1].message)
 	
 	def test_deprecated_assignment(self):
@@ -49,7 +49,7 @@ class TestAliasDirect:
 			
 			assert len(w) == 1
 			assert issubclass(w[-1].category, DeprecationWarning)
-			assert 'via deprecated' in str(w[-1].message)
+			assert 'via "deprecated"' in str(w[-1].message)
 			assert 'xyzzy' in str(w[-1].message)
 		
 		assert inst.__data__ == {'field': 'bar'}
