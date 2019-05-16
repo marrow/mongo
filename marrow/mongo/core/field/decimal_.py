f7f5ad43ff2582b9a34eb77aac7608dd5077c4b7
Alice Bevan-McGregor <alice@gothcandy.com>
2019-05-16 11:04:10 -0400

    Validation of argument cast, expansion of to_foreign to include anything.
    We offer `to_decimal` and `as_decimal` casting protocols, which can't be (currently) easily tested for by ABC annotation.

--- marrow/mongo/core/field/decimal_.py
+++ marrow/mongo/core/field/decimal_.py
@@ -1,6 +1,6 @@
 from decimal import Decimal as dec, localcontext
 
-from ..types import Union
+from ..types import Any, Union, check_argument_types
 from .number import Number, NumberABC
 
 try:
@@ -15,17 +15,15 @@ else:
 		
 		DECIMAL_CONTEXT = create_decimal128_context()
 		
-		def to_native(self, obj, name:str, value:Union[str,dec,Decimal128]) -> dec:  # pylint:disable=unused-argument
-			if isinstance(value, dec):
-				return value
-			
-			if isinstance(value, Decimal128):
-				value = str(value)
+		def to_native(self, obj, name:str, value:Union[str,dec,Decimal128,float]) -> dec:  # pylint:disable=unused-argument
+			assert check_argument_types()
 			
 			with localcontext(self.DECIMAL_CONTEXT) as ctx:
-				return ctx.create_decimal(value)
+				return ctx.create_decimal(str(value))
 		
-		def to_foreign(self, obj, name:str, value:Union[str,dec]) -> Decimal128:  # pylint:disable=unused-argument
+		def to_foreign(self, obj, name:str, value:Union[str,dec,Decimal128,float,Any]) -> Decimal128:  # pylint:disable=unused-argument
+			assert check_argument_types()
+			
 			if hasattr(value, 'to_decimal'):
 				value = value.to_decimal()
 			
