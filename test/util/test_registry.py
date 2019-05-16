916d32670b67b48637754de9a9454cdc91d58f13
Alice Bevan-McGregor <alice@gothcandy.com>
2019-05-16 09:18:12 -0400

    Include some basic plugin registry tests.

---
+++ test/util/test_registry.py
@@ -0,0 +1,24 @@
+from marrow.mongo import Document, document, field, trait
+
+
+class TestRegistryBehaviour:
+	def test_registry_namespaces(self):
+		assert document._namespace == 'marrow.mongo.document'
+		assert field._namespace == 'marrow.mongo.field'
+		assert trait._namespace == 'marrow.mongo.trait'
+	
+	def test_plugin_existence_check(self):
+		assert 'Document' in document
+		assert 'XYZZY' not in document
+		assert 'Field' in field
+		assert 'String' in field
+		assert 'Bob' not in field
+		assert 'Identified' in trait
+		assert 'Localized' in trait
+		assert 'Explosive' not in trait
+	
+	def test_registry_directory(self):
+		plugins = dir(document)
+		assert len(plugins) >= 1
+		assert 'Document' in plugins  # A plugin.
+		assert '__path__' in plugins  # Not a plugin, but a valid attribute.
