3bab02e9f8eaae3195f2eaeadfae5f7349e73430
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-07-20 11:20:06 -0400

    Import corrections for modern Python 3.

--- marrow/mongo/core/field/mapping.py
+++ marrow/mongo/core/field/mapping.py
@@ -1,4 +1,5 @@
-from collections import OrderedDict, Mapping as _Mapping
+from collections import OrderedDict
+from collections.abc import Mapping as _Mapping
 
 from ....schema import Attribute
 from .array import Array
